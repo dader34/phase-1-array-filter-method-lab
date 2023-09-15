6ae8360f93c7da4636d54d965e2a190a5694772b
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-09-15 16:49:08 -0700

    Passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+const findMatching = (driversArray,passedName) =>{
+    const matching = driversArray.filter((person) => {
+        return (person.toLowerCase() === passedName.toLowerCase())
+    })
+    return matching
+}
+
+const fuzzyMatch = (driversArray,passedName) =>{
+    const matching = driversArray.filter((person)=>{
+        const len = passedName.length
+        return(person.substring(0,len).toLowerCase() === passedName.toLowerCase())
+    })
+    return matching
+}
+
+const matchName = (driversArray,passedName) =>{
+    const matching = driversArray.filter((person)=>{
+        if(person.name.toLowerCase() === passedName.toLowerCase()){
+            return person
+        }
+    })
+    return matching
+}
